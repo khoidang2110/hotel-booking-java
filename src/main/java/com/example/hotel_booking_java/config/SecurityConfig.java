d18254581650743f604d993b8a69e51a096c80ab
“khoidang2110” <khoidang2110@gmail.com>
2025-04-11 18:25:32 +0700

    /

--- src/main/java/com/example/hotel_booking_java/config/SecurityConfig.java
+++ src/main/java/com/example/hotel_booking_java/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                 .cors(cors ->cors.configurationSource(corsConfigurationSource))
                 .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request-> {
-                    request.requestMatchers("/auth/login","/user/sign-up","/upload","/product/**","/files/**").permitAll();
+                    request.requestMatchers("/auth/login","/user/sign-up","/upload","/product/**","/files/**","/user/info").permitAll();
                     request.anyRequest().authenticated();
                 })
                 .addFilterBefore(customAuthenFilter, UsernamePasswordAuthenticationFilter.class)
